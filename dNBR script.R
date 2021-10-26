83bc8ccbcc112ec9cf59d0830b8e5b2bfef28647
hfertel <59543630+hfertel@users.noreply.github.com>
2021-10-26 14:47:58 -0700

    commit2

---
+++ dNBR script.R
@@ -0,0 +1,41 @@
+## ---------------------------
+##
+## Script name: Shrub Severity dNBR script 
+##
+## Purpose of script:
+##
+## Author: Hannah Fertel
+##
+## Date Created: 2021-10-26
+##
+## Copyright (c) Hannah Fertel, 2021
+## Email: hmfertel@berkeley.edu
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
+## ---------------------------
+
+options(scipen = 6, digits = 4) # I prefer to view outputs in non-scientific notation
+memory.limit(30000000)     # this is needed on some PCs to increase memory allowance, but has no impact on macs.
+
+## ---------------------------
+
+## load up the packages we will need:  (uncomment as required)
+
+require(tidyverse)
+require(data.table)
+# source("functions/packages.R")       # loads up all the packages we need
+
+## ---------------------------
+
+## load up our functions into memory
+
+# source("functions/summarise_data.R") 
+
+## ---------------------------
